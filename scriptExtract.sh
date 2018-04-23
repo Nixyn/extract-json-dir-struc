81ce5e976313db9f9e2f47050dfcca60d2df94c2
Eleazar1991 <eleazar.yio@gmail.com>
2018-04-23 13:11:17 +0100

    Terminado segun en que SO se vaya a ejecutar cambiar el path en el script donde se escribe el json

--- scriptExtract.sh
+++ scriptExtract.sh
@@ -7,12 +7,14 @@ function recorrer_directorio {
 		if [ -d $file ];then
 			cd $file
 			recorrer_directorio ./
+			cd ..
 		elif [ -f $file ];then
 			busqueda=`cat $file | grep "Datos Personales"` 
 			if [ ! -z "$busqueda" ];then
-				NOMBRE=`cat $file | grep -E 'Nombre:* | nombre:*'`
-				DNI=`cat $file | grep -E 'Dni:* | dni:*'`
- 				CORREO=`cat $file | grep -E 'Correo:* | correo:* | email:* | Email:*'`							
+				NOMBRE=`cat $file | grep -E 'Nombre:* | nombre:*' | cut -d ':' -f 2 | cut -f 2-4 -d ' '`
+				DNI=`cat $file | grep -E 'Dni:* | dni:*' | cut -d ':' -f 2 | cut -f 2 -d ' '`
+ 				CORREO=`cat $file | grep -E 'Correo:* | correo:* | email:* | Email:*' | cut -d ':' -f 2 | cut -f 2 -d ' '`
+				echo "{ \"name\":\""$NOMBRE"\", \"dni\":"\"$DNI"\", \"email\":"\"$CORREO"\"}" >> ~/Escritorio/users.json							
 			fi	 				
 		fi
 	done			
@@ -23,5 +25,6 @@ DIR=$1
 if [ -d $1 ];then
         cd $DIR
         recorrer_directorio $DIR
+	cat ~/Escritorio/users.json
 fi
 
